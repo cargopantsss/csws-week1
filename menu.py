79ae509bc2b57f0a08f48102dd532de203d3ccf8
cargopantsss <75589913+cargopantsss@users.noreply.github.com>
2022-02-15 15:18:44 +0000

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,17 +1,17 @@
-
+#imports---------------------------------------------------------------------------------------------------------
+from tkinter import *
 #page one--------------------------------------------------------------------------------------------------------
 def page_one():
     key = 0
-    print ("""------------------------------------------
-THIS IS PAGE ONE""")
+    print ("""-------------------PAGE1-------------------""")
     while key == 0:
-        exit = input("""CLICK "0" TO RETURN TO MAIN MENU: """)
-        if exit == 0 or exit == 0:
+        choice = input("""Please choose a page option: """)
+        if choice == "0":
             key = 1
             menu()
         else:
             print ("Invalid input")
-            page_one()
+#page two-------------------------------------------------------------------------------------------------------
 def page_two():
     import csv
     key = 0
@@ -20,17 +20,42 @@ def page_two():
         reader = csv.reader(csvfile)
         data = list(reader)
         print ("""-------------------PAGE2-------------------
-    1) parce data
-    2) exit
+    1) Parce Data
+    0) EXIT
     """)
     while key == 0:
-        parce = input("""Please choose a page option: """)
-        if parce == "1":
-            print(data)
-            
-        elif parce == "2":
+        choice = input("""Please choose a page option: """)
+        if choice == "0":
             key = 1
             menu()
+        elif choice == "1":
+            print(data)
+            
+        
+        else:
+            print ("Invalid input")
+#page three------------------------------------------------------------------------------------------------------
+def page_three():
+    key = 0
+    print("""-------------------PAGE3-------------------
+1)Visual Representation
+2)Written Representation
+0)EXIT
+""")
+    while key == 0:
+        choice = input("""Please choose a page option: """)
+        if choice == "0":
+            key = 1
+            menu()
+        elif choice == "1":
+            print("Diagram has opened")
+            window = Tk()
+            window.title("test")
+            window.geometry("250x250")
+            window.mainloop()
+        elif choice == "2":
+            print("Text based data")
+        
         else:
             print ("Invalid input")
     
@@ -38,23 +63,35 @@ def page_two():
 #main menu-------------------------------------------------------------------------------------------------------    
 def menu():
     print ("""-------------------MENU-------------------
-    1) page test
-    2) csv test
-    3) ...
-    4) EXIT
+    1) Page Test
+    2) Csv Test
+    3) Tkinter Test
+    4) ...
+    5) ...
+    6) ...
+    0) EXIT
     """)
     main_choice()
 def main_choice():   
     choose = input("Please choose a menu option: ")
-    if choose == 1:
+    if choose == "1":
         print ("001")
         page_one()
-    elif choose == 2:
+    elif choose == "2":
         print ("010")
         page_two()
-    elif choose == 3:
+    elif choose == "3":
         print ("011")
-    elif choose == 4:
+        page_three()
+    elif choose == "4":
+        print ("100")
+        page_one()
+    elif choose == "5":
+        print ("101")
+        page_two()
+    elif choose == "6":
+        print ("110")
+    elif choose == "0":
         exit()
     else:
         print ("error")
